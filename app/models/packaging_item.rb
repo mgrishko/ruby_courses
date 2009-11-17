e5bf964e951a12a0ca3e9d026f8492c5d09cd7b0
Olexiy Zamkoviy <olexiy.z@gmail.com>
2009-11-17 22:19:59 +0200

    Added fields for packaging item see #41

--- app/models/packaging_item.rb
+++ app/models/packaging_item.rb
@@ -8,8 +8,18 @@ class PackagingItem < ActiveRecord::Base
   belongs_to :user
   validates_is_gtin :gtin
   validates_presence_of :gtin
-  validates_numericality_of :gtin
+  validates_numericality_of :gtin, :less_than => 10 ** 14, :greater_than_or_equal_to => (10 ** (14 - 1))
   validates_uniqueness_of :gtin, :scope => :user_id
+
+
+  validates_numericality_of :number_of_next_lower_item, :less_than_or_equal_to => 999999, :greater_than => 0
+  validates_numericality_of :number_of_bi_items, :less_than_or_equal_to => 999999, :greater_than => 0
+  validates_numericality_of :gross_weight, :less_than => 10 ** 7, :greater_than => 0
+  validates_numericality_of :packaging_type
+  validates_numericality_of :height, :less_than => 10 ** 5, :greater_than => 0
+  validates_numericality_of :depth, :less_than => 10 ** 5, :greater_than => 0
+  validates_numericality_of :width, :less_than => 10 ** 5, :greater_than => 0
+
 
   def name
     item_name_long_ru
--- test/unit/packaging_item_test.rb
+++ test/unit/packaging_item_test.rb
@@ -19,4 +19,15 @@ class PackagingItemTest < ActiveSupport::TestCase
     assert_equal f.name, test_value
     assert_equal f.item_name_long_ru, test_value
   end
+
+  should_validate_numericality_of :number_of_next_lower_item, :number_of_bi_items, :gross_weight, :packaging_type, :height, :depth, :width
+  should_ensure_value_in_range :gtin, (10 ** (14 - 1))..(10 ** 14 - 1), :low_message => /must be greater than/, :high_message => /must be less than/
+  should_ensure_value_in_range :gross_weight, (1..9999999), :low_message => /must be greater than/, :high_message => /must be less than/
+  should_ensure_value_in_range :number_of_next_lower_item, (1..999999), :low_message => /must be greater than/, :high_message => /must be less than/
+  should_ensure_value_in_range :number_of_bi_items, (1..999999), :low_message => /must be greater than/, :high_message => /must be less than/
+  should_ensure_value_in_range :depth, (1..99999), :low_message => /must be greater than/, :high_message => /must be less than/
+  should_ensure_value_in_range :height, (1..99999), :low_message => /must be greater than/, :high_message => /must be less than/
+  should_ensure_value_in_range :width, (1..99999), :low_message => /must be greater than/, :high_message => /must be less than/
+
+
 end
