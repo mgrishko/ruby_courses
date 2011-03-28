090340f1266c14a8eba90f41faff09f8cd232ded
vickodin <vick.orel@gmail.com>
2011-03-28 14:02:17 +0400

    new deploy for locum

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,27 +1,49 @@
-set :application, "webforms"
-
-role :app, "178.63.67.133"
-role :db, "178.63.67.133", :primary => true
-role :web, "178.63.67.133"
-
-set :user, 'pyromaniac'
-set :deploy_to, "/home/pyromaniac/projects/#{application}"
-set :scm, :git
+set :application, "aaa"
 set :repository, "file://."
-set :branch, "master"
-set :git_enable_submodules, true
+# Важно!
+# Если развертка будет происходить из локального репозитория,
+# тогда нужно закомментировать следующую сроку:
+set :repository,  "git@git.assembla.com:webforms2.git"
+dpath = "/home/hosting_max2015/projects/datapool"
+
+set :user, "hosting_max2015"
+
+set :use_sudo, false
+set :deploy_to, dpath
 set :deploy_via, :copy
 set :copy_exclude, [".git"]
-set :git_enable_submodules, 1
-set :use_sudo, false
 
-namespace :deploy do
-  task :restart, :roles => :app do
-    run "touch #{current_path}/tmp/restart.txt"
-  end
+set :scm, :git
+
+role :web, "lithium.locum.ru"                          # Your HTTP server, Apache/etc
+role :app, "lithium.locum.ru"                          # This may be the same as your `Web` server
+role :db,  "lithium.locum.ru", :primary => true # This is where Rails migrations will run
+
+after "deploy:update_code", :copy_database_config
+
+task :copy_database_config, roles => :app do
+  db_config = "#{shared_path}/database.yml"
+  run "cp #{db_config} #{release_path}/config/database.yml"
 end
 
-after "deploy:symlink" do
-  run "ln -s #{File.join(shared_path, 'upload/')} #{File.join(current_path, 'public/upload')}"
-  run "ln -s #{File.join(shared_path, 'database.yml')} #{File.join(current_path, 'config/database.yml')}"
+set :unicorn_rails, "/var/lib/gems/1.8/bin/unicorn_rails"
+set :unicorn_conf, "/etc/unicorn/datapool.max2015.rb"
+set :unicorn_pid, "/var/run/unicorn/datapool.max2015.pid"
+
+# - for unicorn - #
+namespace :deploy do
+  desc "Start application"
+  task :start, :roles => :app do
+    run "#{unicorn_rails} -Dc #{unicorn_conf}"
+  end
+
+  desc "Stop application"
+  task :stop, :roles => :app do
+    run "[ -f #{unicorn_pid} ] && kill -QUIT `cat #{unicorn_pid}`"
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "[ -f #{unicorn_pid} ] && kill -USR2 `cat #{unicorn_pid}` || #{unicorn_rails} -Dc #{unicorn_conf}"
+  end
 end
