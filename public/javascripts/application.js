980bcc7d31ba05b1a72045c89dc10f698d65edff
vickodin <vick.orel@gmail.com>
2011-04-07 19:26:15 +0400

    ticket #103 base-items view coding
    Time: 2h00m #103
    test #103

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,11 +2,11 @@ function setHeights() {
   $$('#items .item').each(function(element){
     element.setStyle({minHeight: ''});
   });
-  $$('#items .item').each(function(element){
+  /*$$('#items .item').each(function(element){
     if (!element.up('li').hasClassName('new')) {
       element.setStyle({minHeight: element.up('li').getHeight() - 10 + 'px'});
     }
-  });
+  });*/
 }
 
 Event.observe(window, 'load', setHeights);
