ef3fd70fe132784ebc9f547d30d0b031b5f51a33
Artyom Nikolaev <artyom@a22.in>
2011-04-09 19:11:17 +0800

    #109 migration + part of #110

--- app/helpers/subscription_results_helper.rb
+++ app/helpers/subscription_results_helper.rb
@@ -1,2 +1,14 @@
 module SubscriptionResultsHelper
+  def truncated_comment base_item, n=100
+    comment = base_item.comments.last
+    if comment
+      if comment.content.size > n
+        comment.content[0..n] + '...'
+      else
+        comment.content
+      end
+    else
+      'n/a'
+    end
+  end
 end
