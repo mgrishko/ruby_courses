864a239f33b849ced172b18ed5dde865cb960161
vickodin <vick.orel@gmail.com>
2011-04-11 13:14:13 +0400

    ticket #111 added action, template

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -4,4 +4,14 @@ class SuppliersController < ApplicationController
   def index
     @users = User.paginate :page => params[:page], :per_page => 10, :conditions => {:role => 'supplier'}
   end
+
+  def show
+    @supplier = User.find(params[:id])
+    @base_items = BaseItem.get_base_items :user_id => @supplier.id,
+					  :brand => params[:brand],
+					  :manufacturer_name => params[:manufacturer_name],
+					  :functional => params[:functional],
+					  :tag => params[:tag]
+    get_filters_data_for_base_items
+  end
 end
