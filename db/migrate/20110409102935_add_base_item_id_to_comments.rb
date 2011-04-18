a84aa434a6fc4668e8e40e890b58cde6393ea234
Vitaliy V. Shopov <shopov.vitaliy@gmail.com>
2011-04-19 05:09:44 +0700

    Fix AddBaseItemIdToComments migration

--- db/migrate/20110409102935_add_base_item_id_to_comments.rb
+++ db/migrate/20110409102935_add_base_item_id_to_comments.rb
@@ -1,6 +1,6 @@
 class AddBaseItemIdToComments < ActiveRecord::Migration
   def self.up
-    add_column :comments, :base_item_id, :integer, :null => false
+    add_column :comments, :base_item_id, :integer, :null => false, :default => 0
     Comment.reset_column_information
     Comment.all.each do |comment|
       comment.update_attribute 'base_item_id', BaseItem.first(:conditions => {:item_id => comment.item_id}, :order => "id DESC")
