6a03b9e18daff6205d72743c91a987fbfae75c04
vickodin <vick.orel@gmail.com>
2011-05-20 17:05:53 +0400

    Ticket #173 redirect retailers if they enter in Statuses controller

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,5 +1,8 @@
 class StatusesController < ApplicationController
+  before_filter :require_user
+
   def index
+    redirect_to :controller => "subscription_results" if current_user.retailer?
     @base_items = current_user.all_fresh_base_items_paginate params[:page]
     @users = current_user.retailers.paginate(:page => params[:page_users], :per_page => 10)
   end
