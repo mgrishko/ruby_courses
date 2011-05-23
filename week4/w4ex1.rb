eb1f7802141ff6c69bbadc7c7efe8c9518bc1aa3
MikhailAleksandrovi4 <mikhailaleksandrovi4@gmail.com>
2011-05-23 11:32:01 +0300

    w4ex1

---
+++ week4/w4ex1.rb
@@ -0,0 +1,26 @@
+#Write a class called Dog, that has name as an instance variable and the 
+#following methods:
+
+#bark(), eat(), chase_cat()
+#I shall create the Dog object as follows:
+#d = Dog.new('Leo')
+
+class Dog
+  attr_reader :name, :bark, :eat, :chase_cat
+
+  def initialize(name)
+    @name = name
+  end
+
+  def bark()
+    "gav-gav"
+  end
+
+  def eat()
+    "nam-nam"
+  end
+
+  def chase_cat()
+    "grrr"
+  end
+end
