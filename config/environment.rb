3275b15dce70a043b6f3e3cc0b771b57214c41f9
Pavel <pshegai@gmail.com>
2011-05-30 20:11:43 +0500

    re #189 Add gem selenium-webdriver to environments

--- config/environment.rb
+++ config/environment.rb
@@ -23,6 +23,7 @@ Rails::Initializer.run do |config|
   config.gem "thoughtbot-shoulda", :lib => "shoulda", :source => "http://gems.github.com"
   config.gem "thoughtbot-factory_girl", :lib => false, :source =>  "http://gems.github.com"
   config.gem "capybara"
+  config.gem "selenium-webdriver"
   config.gem "annotate"
 
   # Settings in config/environments/* take precedence over those specified here.
