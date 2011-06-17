b8304196e47288dd264fec5a727ceeaed8c9bf6c
Pavel <pshegai@gmail.com>
2011-06-17 18:14:15 +0500

    update deployment process

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,50 +1,58 @@
-set :application, "aaa"
-set :repository, "file://."
+set :branch, 'rails3'
+dpath = "/var/www/projects/datapool"
 # Важно!
 # Если развертка будет происходить из локального репозитория,
 # тогда нужно закомментировать следующую сроку:
 set :repository,  "git@git.assembla.com:webforms2.git"
-dpath = "/home/hosting_max2015/projects/datapool"
-
-set :user, "hosting_max2015"
-
+set :user, "demo"
+set :password, "Sfer5C"
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
 set :use_sudo, false
+
 set :deploy_to, dpath
 set :deploy_via, :copy
 set :copy_exclude, [".git"]
 
 set :scm, :git
 
-role :web, "lithium.locum.ru"                          # Your HTTP server, Apache/etc
-role :app, "lithium.locum.ru"                          # This may be the same as your `Web` server
-role :db,  "lithium.locum.ru", :primary => true # This is where Rails migrations will run
+role :web, "46.4.115.40"                          # Your HTTP server, Apache/etc
+role :app, "46.4.115.40"                          # This may be the same as your `Web` server
+role :db,  "46.4.115.40", :primary => true # This is where Rails migrations will run
 
 after "deploy:update_code", :copy_database_config
 
 task :copy_database_config, roles => :app do
   db_config = "#{shared_path}/database.yml"
   run "cp #{db_config} #{release_path}/config/database.yml"
-  run "ln -s /home/hosting_max2015/projects/datapool/shared/data #{release_path}/public/data"
+  run "ln -s #{dpath}/shared/data #{release_path}/public/data"
 end
 
-set :unicorn_rails, "/var/lib/gems/1.8/bin/unicorn_rails"
-set :unicorn_conf, "/etc/unicorn/datapool.max2015.rb"
-set :unicorn_pid, "/var/run/unicorn/datapool.max2015.pid"
 
 # - for unicorn - #
 namespace :deploy do
-  desc "Start application"
-  task :start, :roles => :app do
-    run "#{unicorn_rails} -Dc #{unicorn_conf}"
+  task :change_owner_demo do
+    sudo "chown -R demo:demo #{dpath}"
   end
 
-  desc "Stop application"
-  task :stop, :roles => :app do
-    run "[ -f #{unicorn_pid} ] && kill -QUIT `cat #{unicorn_pid}`"
+  task :change_owner_apache do
+    sudo "chown -R apache:apache #{dpath}"
   end
 
-  desc "Restart Application"
-  task :restart, :roles => :app do
-    run "[ -f #{unicorn_pid} ] && kill -USR2 `cat #{unicorn_pid}` || #{unicorn_rails} -Dc #{unicorn_conf}"
+  task :bundle_install do
+    run "cd #{release_path} && rvmsudo bundle install "
   end
+
+  task :start do ; end
+  task :stop do ; end
+
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
 end
+
+#before "deploy:update_code", "deploy:change_owner_demo"
+#after "deploy:update_code", "deploy:change_owner_apache"
+after "deploy:update_code", "deploy:bundle_install"
+
