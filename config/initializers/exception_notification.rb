225c9e73ec27f3447caed219a162b0a4901ab402
Pavel <pshegai@gmail.com>
2011-07-13 17:02:20 +0500

    Add D.Vasilec to exception notification recepients

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -2,6 +2,6 @@ if Rails.env == 'production'
 Webforms::Application.config.middleware.use ExceptionNotifier,
     :email_prefix => "[Exception] ",
     :sender_address => %{"notifier" <server.ror.account@gmail.com>},
-    :exception_recipients => %w{pshegai@gmail.com}
+    :exception_recipients => %w{pshegai@gmail.com pronix.service@gmail.com}
 end
 
