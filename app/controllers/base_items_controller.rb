e4ad561bb3cbf7e13b6dfb29ab2ddc080031e02e
Pavel <pshegai@gmail.com>
2011-07-15 14:17:44 +0500

    пользователь видит только свои данные в autocompletion

--- app/controllers/base_items_controller.rb
+++ app/controllers/base_items_controller.rb
@@ -9,11 +9,16 @@ class BaseItemsController < ApplicationController
   autocomplete :base_item, :manufacturer_gln, :full => true,
                 :extra_data => [:manufacturer_name],
                 :use_limit => true,
-                :display_value => :manufacturer
+                :display_value => :manufacturer do
+                    {:where =>{:user_id =>  current_user.id}}
+                end
   autocomplete :base_item, :manufacturer_name, :full => true,
                 :extra_data => [:manufacturer_gln],
                 :use_limit => true,
-                :display_value => :manufacturer
+                :display_value => :manufacturer do
+                    {:where =>{:user_id =>  current_user.id}}
+                end
+
 
   def index
     redirect_to :controller => "subscription_results" if current_user.retailer?
