ab6a14264a3b2c0dda345a94ff9cc730ca0eafa5
Pavel <pshegai@gmail.com>
2011-07-29 21:23:32 +0500

    re #242 time:40m

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -12,7 +12,6 @@
 class Tag < ActiveRecord::Base
   has_many :items, :through => :clouds
   has_many :clouds
-
-  validates_presence_of :name
+  validate :name, :presence => true, :length => { :within => 1..50 }
 end
 
