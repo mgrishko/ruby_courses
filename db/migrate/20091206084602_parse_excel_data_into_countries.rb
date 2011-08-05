5fe74b96ff7c847309b11ad0d2b6d4be0cb40212
Pavel <pshegai@gmail.com>
2011-08-05 17:23:11 +0500

    fix migration issue

--- db/migrate/20091206084602_parse_excel_data_into_countries.rb
+++ db/migrate/20091206084602_parse_excel_data_into_countries.rb
@@ -9,14 +9,14 @@ class ParseExcelDataIntoCountries < ActiveRecord::Migration
     i = 'a'[0] - 'a'[0]
     k = 'b'[0] - 'a'[0]
 
-    ActiveRecord::Base.transaction do
-      1.upto(worksheet.count - 1) do |n|
-        Country.create(
-          :code => worksheet.cell(n, i).to_s('UTF-8'),
-          :description => worksheet.cell(n, k).to_s('UTF-8')
-        )
-      end
-    end
+    #ActiveRecord::Base.transaction do
+    #  1.upto(worksheet.count - 1) do |n|
+    #    Country.create(
+    #      :code => worksheet.cell(n, i).to_s('UTF-8'),
+    #      :description => worksheet.cell(n, k).to_s('UTF-8')
+    #    )
+    #  end
+    #end
 
   end
 
