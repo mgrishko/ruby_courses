d3e93a563471cedc8f4e49d75a19f77e2afc6437
MikhailAleksandrovi4 <mikhailaleksandrovi4@gmail.com>
2011-08-05 15:41:31 +0300

    repair production

--- db/migrate/20110805052958_add_translates_to_countries.rb
+++ db/migrate/20110805052958_add_translates_to_countries.rb
@@ -4,9 +4,10 @@ class AddTranslatesToCountries < ActiveRecord::Migration
     drop_table :countries
     create_table :countries do |t|
       t.string :code, :limit => 2
+      t.string :description
       t.timestamps
     end
-    Country.create_translation_table! :description => :string
+    #Country.create_translation_table! :description => :string
 
 
     workbook = Spreadsheet::ParseExcel.parse('data/country_codelist_2009-12-05.xls')
@@ -15,18 +16,19 @@ class AddTranslatesToCountries < ActiveRecord::Migration
     skip = 1
     ActiveRecord::Base.transaction do
       worksheet.each(skip) do |row|
-        I18n.locale = :en
+        #I18n.locale = :en
         country = Country.create(
           :code => row.at(0).to_s('UTF-8'),
           :description => row.at(2).to_s('UTF-8')
         )
-        I18n.locale = :ru
-        country.update_attributes(:description => row.at(1).to_s('UTF-8'))
+        #I18n.locale = :ru
+        #country.update_attributes(:description => row.at(1).to_s('UTF-8'))
       end
     end
   end
 
   def self.down
-    Country.drop_translation_table!
+    #Country.drop_translation_table!
+    drop_table :countries
   end
 end
