8e699c3ee111f298f8d3ef63e198d151c1d4d8fe
MikhailAleksandrovi4 <mikhailaleksandrovi4@gmail.com>
2011-08-16 09:16:58 +0300

    fix migration

--- db/migrate/20110513063543_add_replies_to_comments.rb
+++ db/migrate/20110513063543_add_replies_to_comments.rb
@@ -1,8 +1,8 @@
 class AddRepliesToComments < ActiveRecord::Migration
   def self.up
     add_column :comments, :replies, :integer, :null => false, :default => 0
-    execute "create temporary table t as select * from comments"
-    execute "update comments c set replies=(select count(*) from t where root_id=c.id)"
+    #execute "create temporary table t as select * from comments"
+    #execute "update comments c set replies=(select count(*) from t where root_id=c.id)"
   end
 
   def self.down
