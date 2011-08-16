579132c05c9002a53505862decaec4272b5f55ac
Pavel <pshegai@gmail.com>
2011-08-16 16:55:37 +0500

    add mikhailaleksandrovi4@gmail.com and koulikoff@gmail.com to exception notification email list

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -2,6 +2,6 @@ if Rails.env == 'production'
 Webforms::Application.config.middleware.use ExceptionNotifier,
     :email_prefix => "[Exception] ",
     :sender_address => %{"notifier" <server.ror.account@gmail.com>},
-    :exception_recipients => %w{pshegai@gmail.com mrostotski@gmail.com}
+    :exception_recipients => %w{pshegai@gmail.com mrostotski@gmail.com koulikoff@gmail.com mikhailaleksandrovi4@gmail.com}
 end
 
