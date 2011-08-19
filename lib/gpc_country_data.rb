c3491f5bda8c97e7cbe7d537ee4f8515e2d5f332
MikhailAleksandrovi4 <mikhailaleksandrovi4@gmail.com>
2011-08-19 15:42:18 +0300

    Accepted #274 new gpc file & changing parsing gem

--- lib/gpc_country_data.rb
+++ lib/gpc_country_data.rb
@@ -1,4 +1,4 @@
-require 'parseexcel'
+require 'spreadsheet'
 class GpcCountryData
   def initialize
   end
@@ -8,34 +8,37 @@ class GpcCountryData
   end
   def country
     puts 'Filling countries' unless Rails.env.cucumber?  or Rails.env.test?
-    workbook = Spreadsheet::ParseExcel.parse('data/country_codelist_2009-12-05.xls')
-    worksheet = workbook.worksheet(0)
+    Country.delete_all
+    Country.reset_column_information
+    book = Spreadsheet.open('data/country_codelist_2009-12-05.xls')
+    worksheet = book.worksheet(0)
     skip = 1
     ActiveRecord::Base.transaction do
       worksheet.each(skip) do |row|
         Country.create(
-          :code => row.at(0).to_s('UTF-8'),
-          :description => row.at(2).to_s('UTF-8')
+          :code => row.at(0).to_s,
+          :description => row.at(2).to_s
         )
       end
     end
   end
   def gpc
     puts 'Filling GPC' unless Rails.env.cucumber?  or Rails.env.test?
-    workbook = Spreadsheet::ParseExcel.parse('data/gpc_2010-12-01.xls')
-    worksheet = workbook.worksheet(0)
+    Gpc.delete_all
+    Gpc.reset_column_information
+    book = Spreadsheet.open('data/GS1 Combined Published_Schema as at 01062011.xls')
+    worksheet = book.worksheet(0)
     skip = 1
     ActiveRecord::Base.transaction do
       worksheet.each(skip) do |row|
         Gpc.create(
           :code => row.at(6).to_i,
-          :name => row.at(7).to_s('UTF-8'),
-          :group => row.at(3).to_s('UTF-8'),
-          :description => row.at(5).to_s('UTF-8'),
-          :segment_description => row.at(1).to_s('UTF-8')
+          :name => row.at(7).to_s,
+          :group => row.at(3).to_s,
+          :description => row.at(5).to_s,
+          :segment_description => row.at(1).to_s
         )
       end
     end
   end
 end
-
