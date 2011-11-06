37fa1c3f1b33c220c58a318e4ae88927a89c46ff
Mikhail Grishko <mikhailaleksandrovi4@gmail.com>
2011-11-06 22:25:28 +0200

    re #334 time:5h

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
+  #title(name, body => true - optional, if you want to display title in body > h1)
   def title(page_title, options = {})
     if options[:body] == true
-      body_title = content_tag(:h1, page_title.to_s)
+      body_title = content_tag(:h2, page_title.to_s)
       content_for(:body_title, body_title.to_s)
       content_for(:head_title, page_title.to_s)
     else
--- spec/controllers/users/registrations_controller_spec.rb
+++ spec/controllers/users/registrations_controller_spec.rb
@@ -20,4 +20,56 @@ describe Users::RegistrationsController do
 
     it { should render_template(:acknowledgement) }
   end
+
+
+  context "when user is authenticated" do
+    login :user
+
+    describe "GET edit" do
+
+      it "renders edit template" do
+        get :edit
+        response.should render_template(:edit)
+      end
+    end
+
+    describe "PUT edit" do
+
+      describe "failure" do
+
+        before(:each) do
+          attr = { :first_name => "", :last_name => "", :time_zone => "", :email => "",
+                   :locale => "", :current_password => "" }
+        end
+
+        it "should not change user" do
+          #pending
+        end
+
+      end
+
+      describe "success" do
+        before(:each) do
+          @attr = { :first_name => "name", :last_name => "last_name", :time_zone => "Kyiv",
+                   :email => "email@dot.com", :locale => "en", :current_password => "password" }
+        end
+
+        it "should change the user's attributes" do
+          #pending
+        end
+      end
+    end
+  end
+
+  context "when user is authenticated" do
+    logout :user
+
+    describe "GET edit" do
+
+      it "renders edit template" do
+        get :edit
+        response.should_not render_template(:edit)
+      end
+    end
+  end
 end
