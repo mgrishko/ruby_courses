fe5f3261f469a0bf238b1b5e3f4cbf7285e0442e
Vladimir Zhukov <voldyjeengle@gmail.com>
2011-11-10 20:50:34 +0400

    Monkey patched email-spec to visit links by full url (with subdomain).

--- features/support/email_spec.rb
+++ features/support/email_spec.rb
@@ -1,3 +1,15 @@
 # Make sure this require is after you require cucumber/rails/world.
 #require 'email_spec' # add this line if you use spork
 require 'email_spec/cucumber'
+
+
+# Email-spec visits links in email only by path (request_uri). That is not what we want if we use subdomains.
+# This is a monkey patch to use full url instead of path.
+module EmailSpec
+  module Helpers
+    private
+    def request_uri(link)
+      link
+    end
+  end
+end
