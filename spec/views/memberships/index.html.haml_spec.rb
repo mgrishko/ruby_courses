74fc08a6a7f9f65e737706aeb4d1e84a70d1cd29
Andrew Gridnev <andrew.gridnev@gmail.com>
2011-11-15 17:57:23 +0700

    View test ref #342 Time: 0h20m

--- spec/views/memberships/index.html.haml_spec.rb
+++ spec/views/memberships/index.html.haml_spec.rb
@@ -2,18 +2,21 @@ require 'spec_helper'
 
 describe "memberships/index.html.haml" do
   before(:each) do
-    @account = Fabricate(:account_with_memberships)
-    assign(:memberships, @account.memberships)
+    @ability = Object.new()
     
-    @owner_membership = @account.memberships.select{ |m| m.role?(:admin) && m.user == @account.owner }.first
-    @ability = MembershipAbility.new(@owner_membership)
+    @ability.extend(CanCan::Ability)
     @ability.can :manage, Membership
-    controller.stub(:current_ability) {@ability }
+    controller.stub(:current_ability) { @ability }
+    
+    assign(:memberships, [
+      stub_model(Membership, :display_name => "lname", :role_name => "Editor", :model => Membership ),
+      stub_model(Membership, :display_name => "fname", :role_name => "Admin", :model => Membership)
+    ])
   end
 
   it "renders the list of memberships" do
     render
-    rendered.should have_selector("table tr", count: 1 + @account.memberships.count)
+    rendered.should have_selector("table tr", count: 3)
   end
 
   it "renders Name column" do
@@ -24,5 +27,10 @@ describe "memberships/index.html.haml" do
   it "renders Role column" do
     render
     rendered.should have_selector("table th", text: "Role")
+  end
+  
+  it "renders Edit link" do
+    render
+    rendered.should have_selector("table td a", text: "Edit")
   end
 end
