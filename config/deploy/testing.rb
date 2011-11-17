922a7c0ed189f1bda6c3794145caee776711e63e
Vladimir Zhukov <voldyjeengle@gmail.com>
2011-11-18 01:54:00 +0400

    Testing environment for develop server.

--- config/deploy/testing.rb
+++ config/deploy/testing.rb
@@ -3,10 +3,10 @@ ssh_options[:port] = 22 # must be set to open ssh port
 set :deploy_to, "/var/www/projects/#{application}/testing"
 # Deploy to test site from develop branch
 set :branch, "develop"
-set :rails_env, "development"
+set :rails_env, "testing"
 set :user, "demo"
 
-set :appserver, "demo.getmasterdata.com"
+set :appserver, "dev.getmasterdata.com"
 
 role :app, appserver
 role :web, appserver
