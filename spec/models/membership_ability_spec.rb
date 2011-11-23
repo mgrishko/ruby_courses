bde200fc9d2a4a97686a31e209a71a99a2b5a432
Mikhail Grishko <mikhailaleksandrovi4@gmail.com>
2011-11-23 22:57:55 +0200

    re #358 workflow time:8h

--- spec/models/membership_ability_spec.rb
+++ spec/models/membership_ability_spec.rb
@@ -3,24 +3,40 @@ require 'spec_helper'
 
 describe MembershipAbility do
 
+  describe "account owner" do
+    prepare_ability_for :admin, :membership
+
+    before do
+      @owner_id = @resource.account.owner.id
+      @account = Account.first
+      @owner_membership = @account.memberships.owner
+    end
+
+    it {@ability.should be_able_to(:update, Membership, user_id: @owner_id)}
+    it {@ability.should be_able_to(:destroy, Membership, user_id: @owner_id)}
+
+    #it { @ability.should_not be_able_to(:update, @owner_membership) }
+    #it { @ability.should_not be_able_to(:destroy, @owner_membership) }
+  end
+
   describe "account admin" do
     prepare_ability_for :admin, :membership
-    
+
     before do
       @account = Account.first
       @owner_membership = @account.memberships.select{ |m| m.owner? }.first
     end
-  
+
     it { @ability.should_not be_able_to(:update, @owner_membership) }
     it { @ability.should_not be_able_to(:destroy, @owner_membership) }
     it { @ability.should be_able_to(:read, Membership) }
     it { @ability.should be_able_to(:manage, Membership.new) }
     it { @ability.should be_able_to(:manage, Product.new) }
   end
-  
+
   describe "editor" do
     prepare_ability_for :editor, :membership
-    
+
     it { @ability.should_not be_able_to(:read, Membership) }
     it { @ability.should be_able_to(:manage, Product.new) }
   end
