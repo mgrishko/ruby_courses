298988c2dc6ffe3a5772721c8b95dbc09c83b4b6
Mikhail Grishko <mikhailaleksandrovi4@gmail.com>
2011-12-10 18:00:31 +0200

    re #434 add email subject and decorates them time:4h

--- app/mailers/membership_mailer.rb
+++ app/mailers/membership_mailer.rb
@@ -1,14 +1,15 @@
 class MembershipMailer < ActionMailer::Base
   default from: Settings.send_mail_from
 
-  # ToDo Refactor to user AccountDraper
   def invitation_email(membership)
+    set_current_view_context
+    membership = MembershipDecorator.decorate(membership)
     @invited_by = membership.invited_by
     @user = membership.user
     @account = membership.account
     @url = home_url(subdomain: @account.subdomain)
     @invitation_note = membership.invitation_note
 
-    mail( to: @user.email, subject: "Welcome to #{Settings.project_name}" )
+    mail( to: @user.email, subject: "[#{@account.company_name}] Welcome to #{Settings.project_name}" )
   end
 end
