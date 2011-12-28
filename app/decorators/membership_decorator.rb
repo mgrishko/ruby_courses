584ceee7b9a03dac2d04fa480947b4274f602aa6
Mikhail Grishko <mikhailaleksandrovi4@gmail.com>
2011-12-28 19:12:14 +0200

    #434 after rework

--- app/decorators/membership_decorator.rb
+++ app/decorators/membership_decorator.rb
@@ -33,35 +33,32 @@ class MembershipDecorator < ApplicationDecorator
 
   # Sets view content for password label
   #
-  # @param [Hash] optional. if options[:format] == :html it sets password content only for html,
+  # @option [Hash] optional. if options[:format] == :html it sets password content only for html,
   # other, sets for text format
-  #def password_label(options = {})
-    #if membership.user.password.present?
-      #password = membership.user.password
-      ##str = "Password:\n#{membership.user.password}".split("\n")
-      ##str = str.insert(1, "<br>") if options[:format] == :html
-      ##str.join("\n")
-      #if options[:format] == :html
-        #"Password:\n%br\n#{password}"
-      #else
-        #"Password:\n#{password}"
-      #end
-    #end
-  #end
+  def password_label(options = {})
+    if membership.user.password.present?
+      password = membership.user.password
+      if options[:format] == :html
+        "Password:\n<br>\n#{password}".html_safe
+      else
+        "Password:\n#{password}".html_safe
+      end
+    end
+  end
 
   # Sets view content for invitation note
   #
-  # @param [Hash] optional. if options[:format] == :html it sets invitation note only for html,
+  # @option [Hash] optional. if options[:format] == :html it sets invitation note only for html,
   # other, sets for text format
-  #def invitation_label(options = {})
-    #if membership.invitation_note.present?
-      #first_name = membership.invited_by.first_name
-      #invitation_note = membership.invitation_note
-      #if options[:format] == :html
-        #"%p\n  #{first_name}\nalso says:\n%pre= #{invitation_note}"
-      #else
-        #"#{first_name} also says:\n#{invitation_note}"
-      #end
-    #end
-  #end
+  def invitation_label(options = {})
+    if membership.invitation_note.present?
+      first_name = membership.invited_by.first_name
+      invitation_note = membership.invitation_note
+      if options[:format] == :html
+        "<p>\n#{first_name}\nalso says:\n</p><pre>#{invitation_note}</pre>".html_safe
+      else
+        "#{first_name} also says:\n#{invitation_note}".html_safe
+      end
+    end
+  end
 end
