1e4a0cd5e80c56c3a87731ff0eead43810aa5eab
Vladimir Zhukov <voldyjeengle@gmail.com>
2012-01-31 04:04:54 +0400

    Refactored form using bootstrap, refs #601

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,8 @@ module ApplicationHelper
   # Sets content for title in head and for body header.
   #
   # @param [String] page_title page title
-  # @param [Hash] options optional. if options[:body] == true or does not present it sets content for body title.
+  # @param [Hash] options optional.
+  # @option[Symbol] :page_title if true or does not present then it sets content for body title.
   def title(page_title, options = {})
     set_page_title = options[:page_title].nil? ? true : options[:page_title]
     if set_page_title
@@ -29,13 +30,13 @@ module ApplicationHelper
   # @param [String] fieldset legend
   # @param [String] long hint text
   # @param [block] block of inputs
-  def two_col_fieldset(legend, hint, &block)
-    hint_html = content_tag(:div, hint, { class: "large-hint" })
-    hint_wrapper_html = content_tag(:div, hint_html, { class: "span5" })
-    inputs_html = content_tag(:div, capture(&block), { class: "span7 offset1" })
+  def two_col_fieldset(legend, hint = nil, &block)
+    hint_html = content_tag(:div, hint, class: "large-hint")
+    hint_wrapper_html = content_tag(:div, hint_html, class: "span5 columns")
+    inputs_html = content_tag(:div, capture(&block), class: "span8 offset1 columns")
     
     field_set_tag legend do
-      content_tag :div, hint_wrapper_html.concat(inputs_html), { class: "inputs" }
+      content_tag :div, hint_wrapper_html.concat(inputs_html), { class: "row inputs" }
     end
   end
 end
