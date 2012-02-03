5b5caebbfa3ab78ae07b566ada3496d7e454feb0
Vladimir Zhukov <voldyjeengle@gmail.com>
2012-02-03 19:30:14 +0400

    GM-1 added comment for autocomplete action

--- lib/auto_complete/action.rb
+++ lib/auto_complete/action.rb
@@ -1,5 +1,22 @@
 module AutoComplete
   module Action
+
+    # This is a autocomplete controller action.
+    # Model should be set to autocomplete fields (see Mongoid::Searh for details)
+    #
+    # Example:
+    #   In controller:
+    #     class ProductsController < MainController
+    #       include AutoComplete::Action
+    #
+    #   In routes.rb:
+    #     get 'autocomplete/:field' => "products#autocomplete", as: :autocomplete, on: :collection
+    #
+    #   In autocomplete.js.coffe template:
+    #     <% if @values.any? %>
+    #       "<%= @values.join(", ") %>"
+    #     <% end %>
+    #
     def autocomplete
       resource = self.instance_variable_get("@#{controller_name}")
       @field = params[:field].pluralize
