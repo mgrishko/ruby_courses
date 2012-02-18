0302aa5288e0173255781c6611fc3b05319280d1
Mikhail Grishko <mikhail.grishko@gmail.com>
2012-02-18 20:06:40 +0200

    return changes

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-require "rubygems"
 
 # Initialize the rails application
 Webforms::Application.initialize!
-
-Rails::Initializer.run do |config|
-  config.gem :nokogiri
-end
