647b456c68eb37b89933f2085b8b4cedb8c19d98
Mikhail Grishko <mikhail.grishko@gmail.com>
2012-02-18 17:14:52 +0200

    add password, require capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+require "bundler/capistrano"
+
 set :application, "goodsmaster"
 #set :repository, "file://."
 # Важно!
@@ -9,6 +11,7 @@ set :branch, 'rails3'
 dpath = "/var/www/projects/goodsmaster"
 
 set :user, "gmadmin"
+set :password, "eR5s2Dem"
 ssh_options[:forward_agent] = true
 default_run_options[:pty] = true
 set :use_sudo, false
@@ -37,7 +40,7 @@ end
 namespace :deploy do
   task :bundle_install do
     run "cd #{release_path} && rvmsudo bundle install "
-    run "chmod 777 .rvm/gems/ree-1.8.7-2012.01/bin/* "
+    sudo "chmod 777 .rvm/gems/ree-1.8.7-2012.01/bin/* "
   end
 
   task :start do ; end
