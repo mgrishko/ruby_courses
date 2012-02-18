753417d4d6fc12f64a9204dd0e98822b6172b36c
Mikhail Grishko <mikhail.grishko@gmail.com>
2012-02-18 19:37:50 +0200

    add rubygems and nokogiri to autoload

--- Rakefile
+++ Rakefile
@@ -1,4 +1,5 @@
 require File.expand_path('../config/application', __FILE__)
+require 'rubygems'
 require 'rake'
 module ::Webforms
   class Application
--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Webforms::Application.initialize!
 
+Rails::Initializer.run do |config|
+  config.gem :nokogiri
+end
