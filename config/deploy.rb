e9c87e733d5d23fc67dddf162e8a1c4415cfbb42
Mikhail Grishko <mikhail.grishko@gmail.com>
2012-02-18 21:05:20 +0200

    add rvm/capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,7 @@
 require "bundler/capistrano"
-set :rvm_ruby_string, 'ree'
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+require "rvm/capistrano"                  # Load RVM's capistrano plugin.
+set :rvm_ruby_string, 'ree'        # Or whatever env you want it to run in.
 
 set :application, "goodsmaster"
 #set :repository, "file://."
