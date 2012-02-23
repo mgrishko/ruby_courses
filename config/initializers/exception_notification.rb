d540aab59b8949e1b1c063bc4a242c934508353f
Mikhail Grishko <mikhail.grishko@gmail.com>
2012-02-23 15:35:34 +0200

    rm voldy from email exceptions

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -2,6 +2,6 @@ if Rails.env == 'production'
 Webforms::Application.config.middleware.use ExceptionNotifier,
     :email_prefix => "[Exception] ",
     :sender_address => %{"notifier" <server.ror.account@gmail.com>},
-    :exception_recipients => %w{mrostotski@gmail.com mikhailaleksandrovi4@gmail.com voldyjeengle@gmail.com}
+    :exception_recipients => %w{mrostotski@gmail.com mikhailaleksandrovi4@gmail.com}
 end
 
